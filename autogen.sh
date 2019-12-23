5e28072a6cce52970fada525edbcf2c11ed9c999
jjmccollum <15383366+jjmccollum@users.noreply.github.com>
2019-12-22 23:47:06 -0500

    Added path configuration to Windows setup in .travis.yml

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,5 @@
 # autogen.sh
 #
 # This script will auto-generate a configure script for checking build environments.
-
-rm -r -f config.cache aclocal.m4
-aclocal #create m4 environment for autotools to use
 autoconf #generate configure script
 #don't call automake as we already have a simple template in Makefile.in

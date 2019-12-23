99502a60a58f53c3c52451079a8149714ed9092c
jjmccollum <15383366+jjmccollum@users.noreply.github.com>
2019-12-22 23:32:28 -0500

    Removed BOM from autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@
 #
 # This script will auto-generate a configure script for checking build environments.
 
-rm -f config.cache aclocal.m4
+rm -r -f config.cache aclocal.m4
 aclocal #create m4 environment for autotools to use
 autoconf #generate configure script
 #don't call automake as we already have a simple template in Makefile.in
